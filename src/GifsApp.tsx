bef7af30c8780e45a0f6af9155a0f5daf2608cfc
MiGaNg <miguelangelyv1@gmail.com>
2025-09-26 17:17:27 -0500

    Estructura inicial, estilos y fuente

---
+++ src/GifsApp.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { mockGifs } from './mock-data/gifs-mock'
+
+export const GifsApp = () => {
+    return (
+        <>
+            <div className='content-center'>
+                <h1>Buscador de Gifs</h1>
+                <p>Descubre y comparte el gif perfecto</p>
+            </div>
+
+            {/* search */}
+            <div className="search-container">
+                <input type="text" placeholder='Buscar gifs' />
+                <button>Buscar</button>
+            </div>
+
+            {/* búsquedas previas */}
+            <div className="previous-searches">
+                <h2>Busquedas previas</h2>
+                <ul className='previous-searches-list'>
+                    <li>Goku</li>
+                    <li>Saitama</li>
+                    <li>Conan</li>
+                </ul>
+            </div>
+
+            {/* Gifs */}
+            <div className="gifs-container">
+                {
+                    mockGifs.map((gif) => (
+                        <div key={gif.id} className='gif-card'>
+                            <img src={gif.url} alt={gif.title} />
+                            <h3>{gif.title}</h3>
+                            <p>
+                                {gif.width} x {gif.height} (1.5mb)
+                            </p>
+                        </div>
+                    ))
+                }
+            </div>
+        </>
+    )
+}
