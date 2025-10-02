c9f9ea173f95410463f8d5a86bbbff8ac4e1547a
MiGaNg <miguelangelyv1@gmail.com>
2025-10-01 19:21:00 -0500

    Pruebas en el componente que utiliza el custom hook useCounter

---
+++ src/counter/components/MyCounterApp.test.tsx
@@ -0,0 +1,46 @@
+import { fireEvent, render, screen } from "@testing-library/react"
+import { describe, expect, test } from "vitest"
+import MyCounterApp from "./MyCounterApp"
+
+describe('MyCounterApp', () => {
+    test('should render the component', () => {
+        render(<MyCounterApp />);
+        screen.debug();
+        expect(screen.getByRole('heading', { level: 1 }).innerHTML).toContain(
+            `counter:5`
+        )
+        expect(screen.getByRole('button', { name: '+1' })).toBeDefined();
+        expect(screen.getByRole('button', { name: '-1' })).toBeDefined();
+        expect(screen.getByRole('button', { name: 'Reset' })).toBeDefined();
+    })
+
+    test('should increment the counter', () => {
+        render(<MyCounterApp />);
+
+        const labelH1 = screen.getByRole('heading', { level: 1 });
+        const button = screen.getByRole('button', { name: '+1' });
+
+        fireEvent.click(button);
+        expect(labelH1.innerHTML).toBe('counter:6');
+    })
+
+    test('should decrement the counter', () => {
+        render(<MyCounterApp />);
+
+        const labelH1 = screen.getByRole('heading', { level: 1 });
+        const button = screen.getByRole('button', { name: '-1' });
+
+        fireEvent.click(button);
+        expect(labelH1.innerHTML).toBe('counter:4');
+    })
+
+    test('should reset the counter', () => {
+        render(<MyCounterApp />);
+
+        const labelH1 = screen.getByRole('heading', { level: 1 });
+        const button = screen.getByRole('button', { name: 'Reset' });
+
+        fireEvent.click(button);
+        expect(labelH1.innerHTML).toBe('counter:5');
+    })
+})
