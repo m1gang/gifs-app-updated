7711cd686cd32dc1e122c198aab1d7d9ceea68ed
MiGaNg <miguelangelyv1@gmail.com>
2025-10-03 15:46:59 -0500

    Pruebas en componente SearchBar: Pruebas adicionales sobre efectos

--- src/shared/components/SearchBar.test.tsx
+++ src/shared/components/SearchBar.test.tsx
@@ -27,5 +27,43 @@ describe('SearchBar', () => {
 
     })
 
+    test('should call only once with the las value (debounce)', async () => {
+        const onQuery = vi.fn();
+        render(<SearchBar onQuery={onQuery} />);
+
+        const input = screen.getByRole('textbox');
+        fireEvent.change(input, { target: { value: 't' } });
+        fireEvent.change(input, { target: { value: 'te' } });
+        fireEvent.change(input, { target: { value: 'test' } });
+        await waitFor(() => {
+            expect(onQuery).toHaveBeenCalledTimes(1);
+            expect(onQuery).toHaveBeenCalledWith('test');
+        })
+    })
+
+    test('should call onQuery when button clicked with the input value', () => {
+        const onQuery = vi.fn();
+        render(<SearchBar onQuery={onQuery} />);
+
+        const input = screen.getByRole('textbox');
+        fireEvent.change(input, { target: { value: 'test' } });
+
+        const button = screen.getByRole('button');
+        fireEvent.click(button);
+
+        expect(onQuery).toHaveBeenCalledTimes(1);
+        expect(onQuery).toHaveBeenCalledWith('test');
+    })
+
+    test('should the input has the correct placeholder value', () => {
+
+        const value = 'Buscar gif';
+
+        render(<SearchBar onQuery={() => { }} placeholder={value} />);
+        expect(screen.getByPlaceholderText(value)).toBeDefined();
+    })
+
+
+
 
 })
